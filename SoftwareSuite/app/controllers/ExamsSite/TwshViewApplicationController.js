c3447172907992b2e25942a7d2df653fe45fee9b
mukul <mukul@hebeon.com>
2022-03-08 16:41:24 +0530

    date format changes

--- SoftwareSuite/app/controllers/ExamsSite/TwshViewApplicationController.js
+++ SoftwareSuite/app/controllers/ExamsSite/TwshViewApplicationController.js
@@ -299,7 +299,7 @@
                     //    }
 
                     var myDate = new Date();
-                    myDate = $filter('date')(obj.date, "yyyy-MM-dd");
+                    myDate = $filter('date')(obj.date, "dd-MM-yyyy");
 
                     return myDate;
                 });
