1ae4d29852d842347882c37a05f9929b3d2378c1
T-sindhu <sindhuupavani@gmail.com>
2024-02-21 17:02:00 +0530

    SindhuBranch

--- SoftwareSuite/app/controllers/CBT/TestingTwshStudentRegController.js
+++ SoftwareSuite/app/controllers/CBT/TestingTwshStudentRegController.js
@@ -2153,9 +2153,10 @@
 
                 if (response.Table[0].ResponceCode == '200') {
                     alert(response.Table[0].ResponceDescription);
+                    $scope.ApplicationNo = response.Table1[0].ApplicationNumber;
                     $scope.PreviewData = false;
                     $scope.Reset1();
-
+                    $scope.applicationsuccess = true;
                 }
 
                 else if (response.Table[0].ResponceCode == '400') {
