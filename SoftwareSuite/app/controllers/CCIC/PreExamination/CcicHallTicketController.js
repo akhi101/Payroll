df5750d57cd886be325542c726dada33d64d50e1
akhi101 <b.akhil101@gmail.com>
2024-03-07 11:15:26 +0530

    CCicHT

--- SoftwareSuite/app/controllers/CCIC/PreExamination/CcicHallTicketController.js
+++ SoftwareSuite/app/controllers/CCIC/PreExamination/CcicHallTicketController.js
@@ -1,58 +1,66 @@
 ﻿define(['app'], function (app) {
     app.controller("CcicHallTicketController", function ($scope, $localStorage, CcicPreExaminationService) {
         var authData = $localStorage.authorizationData;
-        var tmpData = $localStorage.TempData;
+        //  var tmpData = $localStorage.TempData;
+        var AcademicYearID = sessionStorage.getItem("AcademicYearID");
+        var ExamMonthYearID = sessionStorage.getItem("ExamMonthYearID");
+        var StudentID = sessionStorage.getItem("StudentID");
+        var StudentType = sessionStorage.getItem("StudentType");
+
         const $ctrl = this;
         $ctrl.$onInit = () => {
-            $scope.getHallTkt ()
+
+
+            $scope.getHallTkt()
         }
 
 
-        
+
 
         $scope.getHallTkt = function () {
-           
-           
-            var getHallticket = CcicPreExaminationService.GetCandidateHallticket(tmpData.AcademicYearID, tmpData.ExamMonthYearID, tmpData.StudentTypeID, tmpData.StudentID);
+
+            console.log(AcademicYearID, ExamMonthYearID, StudentType, StudentID)
+            var getHallticket = CcicPreExaminationService.GetCandidateHallticket(AcademicYearID, ExamMonthYearID, StudentType, StudentID);
             getHallticket.then(function (resp) {
                 var resp = JSON.parse(resp);
                 if (resp.Table !== undefined) {
                     if (resp.Table[0].StatusCode == '200') {
-                            $scope.result = true;
-                            $scope.studPin = resp.Table1[0].PIN;
-                            $scope.Photo = resp.Table1[0].StudentPhoto;
-                            $scope.studentName = resp.Table1[0].StudentName;
-                            $scope.studentFatherName = resp.Table1[0].FatherName;
-                            $scope.studentCourse = resp.Table1[0].CourseName;
-                            $scope.studentExamCenterName = resp.Table1[0].ExaminationCentreName;
-                            $scope.studentExamCenterCode = resp.Table1[0].ExaminationCentreCode;
-                            $scope.studentInsName = resp.Table1[0].InstitutionName;
-                            $scope.examMonthYear = resp.Table1[0].ExamMonthYear;
-                            $scope.studentSubData = resp.Table2;
-                            $scope.loading = false;
-                            $scope.Noresult = false;
-                            $scope.result = true;
-                            //if ($scope.Photo == '' || $scope.Photo == null) {
-                            //    alert("Photo is not available, please upload photo from your college and download Hall Ticket.")
-                            //    $scope.result = false;
-                            //    $scope.LoadImg = false;
-                            //}
 
-                            // alert("No Student found with this Record");
-                        
-                        
-                        } else if (resp.Table[0].ResponceCode == '400') {
-                            $scope.result = false;
-                            alert(resp.Table[0].ResponceDescription);
-                        } else {
-                            $scope.result = false;
-                            $scope.LoadImg = false;
-                            $scope.DetailsFound = false;
-                            $scope.DetailsNotFound = true;
+                        $scope.result = true;
+                        $scope.studPin = resp.Table1[0].PIN;
+                        $scope.Photo = resp.Table1[0].StudentPhoto;
+                        $scope.studentName = resp.Table1[0].StudentName;
+                        $scope.studentFatherName = resp.Table1[0].FatherName;
+                        $scope.studentCourse = resp.Table1[0].CourseName;
+                        $scope.studentExamCenterName = resp.Table1[0].ExaminationCentreName;
+                        $scope.studentExamCenterCode = resp.Table1[0].ExaminationCentreCode;
+                        $scope.studentInsName = resp.Table1[0].InstitutionName;
+                        $scope.examMonthYear = resp.Table1[0].ExamMonthYear;
+                        $scope.studentSubData = resp.Table2;
+                        $scope.loading = false;
+                        $scope.Noresult = false;
+                        $scope.result = true;
+                        //if ($scope.Photo == '' || $scope.Photo == null) {
+                        //    alert("Photo is not available, please upload photo from your college and download Hall Ticket.")
+                        //    $scope.result = false;
+                        //    $scope.LoadImg = false;
+                        //}
 
-                        }
+                        // alert("No Student found with this Record");
+
+
+                    } else if (resp.Table[0].ResponceCode == '400') {
+                        $scope.result = false;
+                        alert(resp.Table[0].ResponceDescription);
+                    } else {
+                        $scope.result = false;
+                        $scope.LoadImg = false;
+                        $scope.DetailsFound = false;
+                        $scope.DetailsNotFound = true;
+
                     }
-                
+                }
+
                 else {
                     alert("No Hallticket founded");
                 }
